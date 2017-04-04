a2440285610aea98e3ffdc81a2c9f6ac143cc8b7
vangheem <vangheem@gmail.com>
2017-04-04 15:04:56 -0500

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = [
 ]
 
 setup(name='guillotina_swagger',
-      version='1.0.0',
+      version='1.0.1.dev0',
       description='Swagger integration for viewing REST on your guillotina install',
       long_description=(open('README.rst').read() + '\n' +
                         open('CHANGES.rst').read()),
