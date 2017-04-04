fd0ef59817c7ebdbf3f9a3dbd1ff99438604a290
vangheem <vangheem@gmail.com>
2017-04-04 15:02:57 -0500

    update readme and manifiest

--- setup.py
+++ setup.py
@@ -10,9 +10,19 @@ setup(name='guillotina_swagger',
       description='Swagger integration for viewing REST on your guillotina install',
       long_description=(open('README.rst').read() + '\n' +
                         open('CHANGES.rst').read()),
+      keywords=['guillotina', 'REST', 'swagger'],
       author='Nathan Van Gheem',
       author_email='vangheem@gmail.com',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
+      classifiers=[
+          'License :: OSI Approved :: BSD License',
+          'Intended Audience :: Developers',
+          'Topic :: Internet :: WWW/HTTP',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      url='https://github.com/guillotinaweb/guillotina_swagger',
+      license='BSD',
       install_requires=requires)
